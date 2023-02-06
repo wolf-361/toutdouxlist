4ff95491e3b747080b198885bad030b14dc5196c
Luc <wolf-361@hotmail.com>
2023-02-06 15:32:40 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.todolist',
+  appName: 'todolist',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
