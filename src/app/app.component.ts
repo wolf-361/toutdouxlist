59be2ff1b13281098d40bf66421fbec1b7bf7c92
Luc <wolf-361@hotmail.com>
2023-02-08 11:02:49 -0500

    ajout d'un vrais dark theme (celui recommander) sans toggle fonctionnel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+
+  prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
+
+  constructor() {
+    this.prefersDark.addEventListener('change', (e) => console.log(e));
+  }
+
+  toggleDarkTheme(shouldAdd: boolean) {
+    document.body.classList.toggle('dark', shouldAdd);
+    console.log('dark theme toggled');
+  }
+
 }
